6f578a60c4ec944bb5221e92bf6bd428243b2113
lanxuage <zx1456817554@gmail.com>
2023-05-10 16:26:12 +0800

    Update

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -ex
-sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
+# sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
 apk update
 apk add linux-headers musl-dev gcc go libpcap-dev ca-certificates git
 mkdir /go
 export GOPATH=/go
 go env -w GO111MODULE=on
-go env -w GOPROXY=https://goproxy.cn,direct
+# go env -w GOPROXY=https://goproxy.cn,direct
 cd /mnt
 go mod tidy
 env CGO_ENABLED=1 go build --ldflags "${LDFLAGS}" -o ${DIRECTORY}/gscan-${GOOS}-${GOARCH}${SUFFIX} cli/main.go
